4a266b941a95f2779863da236d70ce515f6ce782
Ben Awad <benawad97@gmail.com>
2019-01-03 20:03:55 -0600

    basic graphql types

--- src/index.js
+++ src/index.js
@@ -1,14 +1,72 @@
 const { ApolloServer, gql } = require("apollo-server");
 
+// type checking
+// query vs. mutation
+// objects
+// arrays
+// arguments
+
+// crud
+
 const typeDefs = gql`
   type Query {
-    hello: String!
+    hello: String
+    user: User
+  }
+
+  type User {
+    id: ID!
+    username: String!
+  }
+
+  type Error {
+    field: String!
+    message: String!
+  }
+
+  type RegisterResponse {
+    errors: [Error!]!
+    user: User
+  }
+
+  input UserInfo {
+    username: String!
+    password: String!
+    age: Int
+  }
+
+  type Mutation {
+    register(userInfo: UserInfo!): RegisterResponse!
+    login(userInfo: UserInfo!): Boolean!
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "Hello World!"
+    hello: () => null,
+    user: () => ({
+      id: 1,
+      username: "bob"
+    })
+  },
+  Mutation: {
+    login: () => true,
+    register: () => ({
+      errors: [
+        {
+          field: "username",
+          message: "bad"
+        },
+        {
+          field: "username2",
+          message: "bad2"
+        }
+      ],
+      user: {
+        id: 1,
+        username: "bob"
+      }
+    })
   }
 };
 
